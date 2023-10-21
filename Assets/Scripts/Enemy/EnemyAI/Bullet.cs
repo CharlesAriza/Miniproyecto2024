bd8211626f0afe7d53dc0246bca7df8a1e97d977
Albert <albertcharlesariza@gmail.com>
2023-10-21 15:40:29 +0200

    Complex AI Implemented
    + Complex AI Implemented
    
    - Bug: Player colliders seem to be buggy and Raycast dosent perform well.

---
+++ Assets/Scripts/Enemy/EnemyAI/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform hitTransform = collision.transform;
+        if (hitTransform.CompareTag("Player"))
+        {
+            Debug.Log("Hit Player");
+            //How many damage takes the player.
+            hitTransform.GetComponent<PlayerHealth>().TakeDamage(30);
+        }
+        Destroy(gameObject);
+    }
+}
