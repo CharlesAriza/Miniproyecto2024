aac5398b6ce7b1dbef70e000b0f3fc3e906d59de
Albert Charles <albertcharlesariza@gmail.com>
2024-03-13 17:37:18 +0100

    A* en incorporado

---
+++ Assets/Scripts/Enemy/EnemyAI/PathAstar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathAstar : MonoBehaviour
+{
+    [SerializeField] List<Path> list = new List<Path>();
+
+}
+
--- Assets/Scripts/Enemy/EnemyAI/StateMachine.cs
+++ Assets/Scripts/Enemy/EnemyAI/StateMachine.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class StateMachine : MonoBehaviour
 {
     public BaseState activeState;
-
+    //Propiedad de la patruya.
     public void Initialise()
     {
-
+        //Creamos un estado base.
         ChangeState(new PatrolState());
     }
     void Start()
@@ -19,22 +19,29 @@ public class StateMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Comprueba si el estado no es nulo.
         if (activeState != null)
         {
+            //Si no es nulo que lo lleve a cabo. 
             activeState.Perform();
         }
 
     }
     public void ChangeState(BaseState newState)
     {
+        //Comprueba que el estado no es nulo.
         if (activeState != null)
         {
+            //Sale del estado actual.
             activeState.Exit();
         }
+        //Cambia al nuevo estado.
         activeState = newState;
         if (activeState != null)
         {
+            //Set el nuevo estado.
             activeState.stateMachine = this;
+            //Asigna el estado al enemigo/agente.
             activeState.enemy = GetComponent<Enemy>();
             activeState.Enter();
         }
